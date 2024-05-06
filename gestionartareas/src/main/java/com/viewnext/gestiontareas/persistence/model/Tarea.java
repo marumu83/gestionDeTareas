ac2062aaba392d72c5790a190fc7f449f1895485
marumu83 <manuel.rubio@viewnext.com>
2024-05-06 16:21:53 +0200

    desarrollo del modelo Tarea, se incluye dependencia Lombok para agilizar la construccion de getter setter y constructores

--- gestionartareas/src/main/java/com/viewnext/gestiontareas/persistence/model/Tarea.java
+++ gestionartareas/src/main/java/com/viewnext/gestiontareas/persistence/model/Tarea.java
@@ -1,8 +1,41 @@
 package com.viewnext.gestiontareas.persistence.model;
+/**
+ * @author Manuel Rubio
+ * @author Javier Garrido
+ * 
+ * Clase que reprensenta le entidad Tarea de la base de datos
+ */
+import java.io.Serializable;
+import java.sql.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-public class Tarea {
-
+@Builder
+public class Tarea implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String titulo;
+	
+	private Date fechaAlta;
+	
+	private Date fechaFin;
+	
+	private String descripcion;
+	
+	private boolean finalizada;
 }
