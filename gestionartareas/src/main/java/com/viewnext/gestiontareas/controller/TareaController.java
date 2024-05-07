1f8c2b25d1a07fabcb9f26181ccb6e8e436bb2ff
Jorge-Garrido-Lopez <jorge.garrido@viewnext.com>
2024-05-07 10:41:45 +0200

    Se añade un HTML y Thymeleaf para el funcionamiento de la Página web

--- gestionartareas/src/main/java/com/viewnext/gestiontareas/controller/TareaController.java
+++ gestionartareas/src/main/java/com/viewnext/gestiontareas/controller/TareaController.java
@@ -3,6 +3,7 @@ package com.viewnext.gestiontareas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +18,21 @@ import com.viewnext.gestiontareas.service.TareaService;
  *
  */
 
-@RestController
+@Controller
 @RequestMapping("/tarea")
 public class TareaController {
 	
 	@Autowired
 	private TareaService tareaService;
 	
+	@GetMapping("/holamundo")
+	public String prueba() {
+		return "HolaMundo";
+	}
+	
+	
 	@GetMapping("/all")
 	public List<Tarea> getAll(){
-		
 		return tareaService.finAll();
 	}
 	
