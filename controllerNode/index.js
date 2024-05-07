a2842fa94c1eaa3096ca12fc95ae805c57e2c1b9
marumu83 <manuel.rubio@viewnext.com>
2024-05-07 16:32:18 +0200

    front js y spring se comunican

--- controllerNode/index.js
+++ controllerNode/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = 8081
+const port = 3000
 
 app.use(
     express.urlencoded({
@@ -26,5 +26,5 @@ app.post('/post', (req,res,) => {
 })
 
 app.listen(port, () => {
-    console.log('Se ejecuta en el puerto el localhost')
+    console.log(`Se ejecuta en el puerto el localhost ${port}`)
 })
--- web/script.js
+++ web/script.js
@@ -6,11 +6,24 @@ formElement.addEventListener("submit", (event) => {
     let titulo = document.getElementById("nombreTarea").value;
     let descripcion = document.getElementById("descripcionTarea").value;
     let fechaFin = document.getElementById("fechaFin").value;
-    let fechaAlta = Date.now().toLocaleString();
+    let fechaAlta = new Date().toISOString().slice(0, 10);
     let finalizada = new Boolean(false);
-
     let tarea = {titulo : titulo, fechaAlta:fechaAlta , fechaFin : fechaFin , descripcion: descripcion, finalizada : finalizada}
     let tareaJson = JSON.stringify(tarea);
-    console.log(tareaJson);    
+    console.log(tareaJson);  
+    
+    fetch('http://localhost:8080/tarea/new', {
+
+        method : 'Post',
+
+            headers : {
+
+                'Accept': 'application/json',
+                'Content-Type' : 'application/json'
+            },
+
+        body : tareaJson
+    })
+    .then(x => console.log(tareaJson))
 
 })
