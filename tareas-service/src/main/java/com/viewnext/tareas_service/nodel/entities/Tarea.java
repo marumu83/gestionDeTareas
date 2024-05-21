b87a25ced23ea8316097ddaec87b565e65b678b3
Jorge-Garrido-Lopez <jorge.garrido@viewnext.com>
2024-05-21 17:04:32 +0200

    Introducimos de tareas el repository y los model

--- tareas-service/src/main/java/com/viewnext/tareas_service/nodel/entities/Tarea.java
+++ tareas-service/src/main/java/com/viewnext/tareas_service/nodel/entities/Tarea.java
@@ -1,2 +1,43 @@
-package com.viewnext.tareas_service.nodel.entities;public class Tarea {
+package com.viewnext.tareas_service.nodel.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tarea")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Tarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String titulo;
+
+    private Date fechaAlta;
+
+    private Date fechaFin;
+
+    private Date ultimaModificacion;
+
+    private String descripcion;
+
+    private boolean finalizada;
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", fechaAlta=" + fechaAlta +
+                ", fechaFin=" + fechaFin +
+                ", ultimaModificacion=" + ultimaModificacion +
+                ", descripcion='" + descripcion + '\'' +
+                ", finalizada=" + finalizada +
+                '}';
+    }
 }
--- tareas-service/src/main/java/com/viewnext/tareas_service/repositories/TareaRepository.java
+++ tareas-service/src/main/java/com/viewnext/tareas_service/repositories/TareaRepository.java
@@ -1,2 +1,7 @@
-package com.viewnext.tareas_service.repositories;public class TareaRepository {
+package com.viewnext.tareas_service.repositories;
+
+import com.viewnext.tareas_service.nodel.entities.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TareaRepository extends JpaRepository<Tarea, Long> {
 }
