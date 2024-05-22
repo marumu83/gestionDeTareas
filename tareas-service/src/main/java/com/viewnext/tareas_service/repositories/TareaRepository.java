e157f84b331c0767d6c38632eb02323c63466a6a
marumu83 <manuel.rubio@viewnext.com>
2024-05-22 10:34:58 +0200

    borrado de carpeta nodel

--- tareas-service/src/main/java/com/viewnext/tareas_service/repositories/TareaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.viewnext.tareas_service.repositories;
-
-import com.viewnext.tareas_service.nodel.entities.Tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TareaRepository extends JpaRepository<Tarea, Long> {
-}
